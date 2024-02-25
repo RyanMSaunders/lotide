912f710be518769552cfc00b5232ce67a0ff5641
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-02-25 20:14:05 +0000

    middle function that returns the middle of an array

---
+++ middle.js
@@ -0,0 +1,52 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = function(arr) {
+  const arrLength = arr.length;
+  let middleOfArr = [];
+  
+  if (arrLength <= 2) {
+    // console.log(middleOfArr);
+    return middleOfArr;
+  } else if (arrLength % 2 !== 0) {
+    let num = (arrLength + 1) / 2;
+    middleOfArr.push(num);
+    // console.log(middleOfArr);
+    return middleOfArr;
+  } else if (arrLength % 2 === 0) {
+    let firstNum = arrLength / 2;
+    let secondNum = firstNum + 1;
+    middleOfArr.push(firstNum, secondNum);
+    // console.log(middleOfArr);
+    return middleOfArr;
+  }
+
+};
+
+
+middle([1]); // => []
+middle([1, 2]); // => []
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
+assertArraysEqual(middle([1, 2, 3]), ([2])); // should pass
+
