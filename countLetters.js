aef2d1fe1503cd8fc5dc8e72b6423574aabb37eb
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-02-26 16:55:37 +0000

    countLetters function that takes string as input and returns object counting the amount of each letter present in string

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(inputStr) {
+  const results = {};
+  for (let letter of inputStr) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+const result1 = countLetters("Ryann");
+
+assertEqual(result1["R"], 1);
+assertEqual(result1["y"], 1);
+assertEqual(result1["a"], 1);
+assertEqual(result1["n"], 2);
+
+console.log(countLetters("Ryan"));
