dce9fdc6088b1d5f21c9b94e6dce0fc2464717a0
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-02-26 17:47:17 +0000

    letterPositions function which takes a string and returns an object whose keys are the letters of that string and whose values are the indices of the occurnces of those letters.

---
+++ letterPositions.js
@@ -0,0 +1,51 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i in sentence){
+    if (sentence[i] != " "){
+      if (results[sentence[i]]){
+        results[sentence[i]].push(Number(i))
+      } else{
+        results[sentence[i]] = [Number(i)]
+      }
+    }
+  }
+  return results;
+};
+
+// console.log(letterPositions("Ryann Saunnders"))
+
+const result1 = letterPositions("Ryann Saunnders");
+
+
+assertArraysEqual( result1["R"], [0]) // => true
+assertArraysEqual( result1["y"], [1]) // => true
+assertArraysEqual( result1["a"], [2, 7]) // => true
+assertArraysEqual( result1["n"], [3, 4, 9, 10]) // => true
+
+assertArraysEqual(letterPositions("hello world").e, [1]);
+assertArraysEqual(letterPositions("hello word").o, [4, 7]);
+
+
+
