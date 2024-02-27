83b771f9585c5865ef86dcd1854239b15b47cf76
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-02-27 01:09:33 +0000

    eqObject that check for equality between two objects and returns a boolean

--- eqObjects.js
+++ eqObjects.js
@@ -28,29 +28,46 @@ const eqObjects = function(object1, object2){
   let object1Values = Object.values(object1);
   let object2Values = Object.values(object2);
 
-
+  if(object1Keys.length !== object2Keys.length) {
+    return false
+  }
   for(let i=0; i < object1Keys.length; i++){
     let valueArrObj1 = Array.isArray(object1Values[i])
     let valueArrObj2 = Array.isArray(object2Values[i])
 
     if(valueArrObj1 == true && valueArrObj2 == true){
-      let eqArrTest = eqArrays(valueArrObj1, valueArrObj2)
+      let eqArrTest = eqArrays(object1Values[i], object2Values[i])
       if(eqArrTest = true){
         return true;
       }
     } 
 
-    if(object1Keys.length !== object2Keys.length){
-      return false
-    } else if(object2Keys.includes(object1Keys[i]) && object1[object1Keys[i]] === object2[object1Keys[i]] ){
-      return true; 
-    } else {
-      return false;
+    if(object2Keys.includes(object1Keys[i])){
+      let obj1ValCount = 0;
+      let obj2ValCount = 0;
+      for(let val1 in object1){
+        if (val1 === object1Keys[i]){
+          obj1ValCount ++
+        }
+      }
+      for(let val2 in object2 ){
+        if(val2 === object1Keys[i])
+        obj2ValCount ++
+      }
+      console.log(obj1ValCount)
+      console.log(obj2ValCount)
+
+      if(obj1ValCount === obj2ValCount){
+        return true
+      }
     }
     
-  }
-
+      return true; 
+  } 
+    
 }
+
+
 
 
 const shirtObject = { color: "red", size: "medium" };
@@ -60,7 +77,7 @@ const anotherShirtObject= { size: "medium", color: "red" };
 const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
 // console.log(eqObjects(shirtObject , longSleeveShirtObject)); // => false
 
-assertEqual(eqObjects(shirtObject, anotherShirtObject), false); // => false
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => false
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
 
 
@@ -74,4 +91,9 @@ assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), tr
 
 
 const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
+
+const multiColorShirtObject2 = { colors: ["red", "blue", "green"], size: "medium" };
+const anotherMultiColorShirtObject2 = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject2  , anotherMultiColorShirtObject2); // => true
+assertEqual(eqObjects(multiColorShirtObject2  , anotherMultiColorShirtObject2), false);
