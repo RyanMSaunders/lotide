e72b4c37543170f005df09e338b8607278f740ca
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-03-12 16:55:05 +0000

    Refactored head.js by seperating tests into another file

--- head.js
+++ head.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(arr){
-  let firstElement = arr[0]
-  return firstElement
+  let firstElement = arr[0];
+  return firstElement;
 };
 
-// TESTS
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 5);
-assertEqual(head([]), 5);
+module.exports = head;
