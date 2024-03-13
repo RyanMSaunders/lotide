278201fe6ac26af474188c34ef871e11d148d10d
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-03-13 15:13:54 +0000

    Refactored to include strictEqual check that array length remains unchanged

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,23 +5,39 @@ const middle   = require('../middle');
 
 describe("#middle", () => {
   it("returns [] for [1]", () => {
-    assert.deepEqual(middle([1]), [] );
+    const arr = [1]
+    assert.strictEqual(arr.length, 1)
+    assert.deepEqual(middle(arr), [] );
+    assert.strictEqual(arr.length, 1)
+
   });
 
   it("returns [] for [1, 2]", () => {
-    assert.deepEqual(middle([1, 2]), [] );
+    const arr = [1, 2]
+    assert.strictEqual(arr.length, 2);
+    assert.deepEqual(middle(arr), [] );
+    assert.strictEqual(arr.length, 2);
   });
 
   it("returns [2] for [1, 2, 3]", () => {
-    assert.deepEqual(middle([1, 2, 3]), [2] );
+    const arr = [1, 2, 3]
+    assert.strictEqual(arr.length, 3);
+    assert.deepEqual(middle(arr), [2] );
+    assert.strictEqual(arr.length, 3);
   });
 
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3] );
+    const arr = [1, 2, 3, 4]
+    assert.strictEqual(arr.length, 4);
+    assert.deepEqual(middle(arr), [2, 3] );
+    assert.strictEqual(arr.length, 4);
   });
 
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4] );
+    const arr = [1, 2, 3, 4, 5, 6]
+    assert.strictEqual(arr.length, 6);
+    assert.deepEqual(middle(arr), [3, 4] );
+    assert.strictEqual(arr.length, 6);
   });
   
   
