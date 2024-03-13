6225698202245bdf735ed77f1ac961a7624bc2fd
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-03-13 15:02:28 +0000

    Refactored using deepEqual to compare arrays

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,26 +3,21 @@ const tail   = require('../tail');
 
 
 describe("#tail", () => {
-  it("returns 2 for result.length", () => {
-    const result = tail(["Hello", "Lighthouse", "Labs"]);
-    assert.strictEqual(result.length, 2);
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"] );
   });
 
-  it("returns 3 for words.length", () => {
-    const words = ["Yo Yo", "Lighthouse", "Labs"];
-    assert.strictEqual(words.length, 3);
+  it("returns ['Yo Yo', 'Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs]", () => {
+    assert.deepEqual(["Yo Yo", "Lighthouse", "Labs"], ["Yo Yo", "Lighthouse", "Labs"] );
   });
 
-  it("returns 3 for words.length", () => {
-    const word = tail(["Yo Yo"]);
-    assert.strictEqual(word.length, 0);
+  it("returns [] for ['Yo Yo']", () => {
+    assert.deepEqual(tail(['Yo Yo']), [] )
   });
 
-  it("returns 3 for words.length", () => {
-    const noWord = tail([]);
-    assert.strictEqual(noWord.length, 0);
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), [] )
   });
-  
 
 });
 
