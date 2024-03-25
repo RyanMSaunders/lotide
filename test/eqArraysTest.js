5656a7d3cd65aac9fe39b26fd5338d4304b181bb
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-03-25 13:50:32 +0000

    Remove commented out code from file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,9 +2,9 @@ const eqArrays = require("../eqArrays");
 const assertEqual = require("../assertEqual");
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true); 
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);// => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
