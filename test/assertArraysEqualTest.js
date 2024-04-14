479b54886bdf8565d0b285c67e1ff344ae816928
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:45:53 +0000

    Lint code and add appropriate comments

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,7 @@
+
+// Function 'assertArraysEqualTest'
 const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) 
-assertArraysEqual([1, 2, 3], [3, 2, 1]) 
+// Testing 'assertArraysEqual'
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
