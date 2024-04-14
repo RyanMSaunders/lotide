6ecbd796a22c9fe5f6511aef7296756370b32e30
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:46:46 +0000

    Lint code and add appropriate comments

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,12 @@
-const eqArrays = require("../eqArrays");
+
+// Test for 'eqArrays'
 const assertEqual = require("../assertEqual");
 
+// Function 'eqArrays'
+const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true); 
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+// Testing 'eqArrays'
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
