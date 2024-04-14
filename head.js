0303f60f352d6aaed3db40c978d29a3e8fa4dea7
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:52:02 +0000

    Lint code and add appropriate comments

--- head.js
+++ head.js
@@ -1,6 +1,9 @@
-const head = function(arr){
+// A function 'head' that returns the first item in an array
+
+const head = function(arr) {
   let firstElement = arr[0];
   return firstElement;
 };
 
+// Exporting 'head'
 module.exports = head;
