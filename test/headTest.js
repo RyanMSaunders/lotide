09b9b78719a130dce69371e69da65a68dac3634a
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:47:01 +0000

    Lint code and add appropriate comments

--- test/headTest.js
+++ test/headTest.js
@@ -1,28 +1,34 @@
+
+// Importing Chai Library
 const assert = require('chai').assert;
-const head   = require('../head');
+
+// Importing head function
+const head = require('../head');
+
+// Tests for 'head' function
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
   });
 
   it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(head([5,6,7]), 5); 
+    assert.strictEqual(head([5,6,7]), 5);
   });
 
   it("returns `Hello` for [`Hello`, `Lighthouse`, `Labs`]", () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
 
   it("does not return 5 for ['2']", () => {
-    assert.notStrictEqual(head(['2']), '5'); 
+    assert.notStrictEqual(head(['2']), '5');
   });
 
   it("returns undefined for []", () => {
-    assert.strictEqual(head([]), undefined); 
+    assert.strictEqual(head([]), undefined);
   });
 
 });
