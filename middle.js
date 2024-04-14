906b715fd1227a052e108d5c1e902bcc1a545635
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:53:13 +0000

    Lint code and add appropriate comments

--- middle.js
+++ middle.js
@@ -1,26 +1,26 @@
 
+// A function 'middle' which will take in an array and return the 
+// middle-most element(s) of the given array.
+
 const middle = function(arr) {
   const arrLength = arr.length;
   let middleOfArr = [];
   
   if (arrLength <= 2) {
-    // console.log(middleOfArr);
     return middleOfArr;
   } else if (arrLength % 2 !== 0) {
     let num = (arrLength + 1) / 2;
     middleOfArr.push(arr[num - 1]);
-    // console.log(middleOfArr);
     return middleOfArr;
   } else if (arrLength % 2 === 0) {
     let firstNum = arrLength / 2;
     let secondNum = firstNum + 1;
     middleOfArr.push(arr[firstNum - 1], arr[secondNum - 1]);
-    // console.log(middleOfArr);
     return middleOfArr;
   }
 
 };
 
-
+// Exporting 'middle'
 module.exports = middle;
 
