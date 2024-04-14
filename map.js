ff8a1a83657f4e37862cfe32c3fbf23740b5e401
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:53:00 +0000

    Lint code and add appropriate comments

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+// Tests for 'map'
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -20,24 +21,30 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-const map = function(array, callback){
-  const results = []
+// A function 'map' which takes array to map and a callback function and returns 
+// a new array based on the results of the callback function.
+
+const map = function(array, callback) {
+  const results = [];
 
   for (let item of array) {
     results.push(callback(item));
   }
-  return results
-}
+  return results;
+};
+
+
+// Testing 'map'
 
 const words = ["ground", "control", "to", "major", "tom"];
 
 const results1 = map(words, word => word[0]);
-console.log(results1);
 
-assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ])
-assertArraysEqual(results1, [ 'g', 'c' ])
-assertArraysEqual(results1, [ 'c', 't', 'm', 't' ])
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]); // => should PASS
+assertArraysEqual(results1, [ 'g', 'c' ]); // => should FAIL
+assertArraysEqual(results1, [ 'c', 't', 'm', 't' ]); // => should FAIL
 
+// Exporting 'map'
 module.exports = map;
 
 
