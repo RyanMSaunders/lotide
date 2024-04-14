35a09066df5d5502baded94194009b0db1968f28
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:47:58 +0000

    Lint code and add appropriate comments

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,27 @@
+
+// Importing Chai Library
 const assert = require('chai').assert;
+
+// Importing 'tail' function
 const tail   = require('../tail');
 
+// Testing 'tail' function
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"] );
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
   it("returns ['Yo Yo', 'Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs]", () => {
-    assert.deepEqual(["Yo Yo", "Lighthouse", "Labs"], ["Yo Yo", "Lighthouse", "Labs"] );
+    assert.deepEqual(["Yo Yo", "Lighthouse", "Labs"], ["Yo Yo", "Lighthouse", "Labs"]);
   });
 
   it("returns [] for ['Yo Yo']", () => {
-    assert.deepEqual(tail(['Yo Yo']), [] )
+    assert.deepEqual(tail(['Yo Yo']), []);
   });
 
   it("returns [] for []", () => {
-    assert.deepEqual(tail([]), [] )
+    assert.deepEqual(tail([]), []);
   });
 
 });
