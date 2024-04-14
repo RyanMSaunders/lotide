3787e7e46725e3e27ddb8c55e7b0970243b6f934
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:53:32 +0000

    Lint code and add appropriate comments

--- tail.js
+++ tail.js
@@ -1,7 +1,10 @@
+// A function tail which returns the "tail" of an array:
+// everything except for the first item (head) of the provided array
+
 const tail = function(arr) {
   let newArr = arr.slice(1);
   return newArr;
 };
 
-
+// Exporting 'tail'
 module.exports = tail;
