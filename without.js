48db6d3e8f2d8a3e14d0a38177e25928893e73b5
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:54:16 +0000

    Lint code and add appropriate comments

--- without.js
+++ without.js
@@ -1,3 +1,4 @@
+// Tests for 'without' function
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -19,36 +20,36 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-// Implement without which will return a subset of a given array,
-//  removing unwanted elements.
-// Write a test case to make sure that the original array is not modified.
+
+// A function 'without' which will return a subset of a given array, removing unwanted elements.
 
 const without = function(source, itemsToRemove) {
-  let newArr = []
+  let newArr = [];
   
-  for(let i in source){
+  for (let i in source) {
       
-        
-    if(!itemsToRemove.includes(source[i])){
-            newArr.push(source[i])
-          } 
-        }
-      return newArr
+    if (!itemsToRemove.includes(source[i])) {
+      newArr.push(source[i]);
     }
-
+  }
+  return newArr;
+};
 
  
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+// Testing 'without' function
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// Making sure the original array was not altered by the without function
+without(words, ["lighthouse"]); 
 
-// want to try: checking for typeOf in conditional statement. creating an else statement, if arr1 is not equal to the unwanted variable, push to empty array
+assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => should PASS
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
 
+
+// Exporting 'without'
 module.exports = without;
 
