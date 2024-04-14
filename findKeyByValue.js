1aea4761ecea9e48db2986beeee80c2ec353ba61
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-14 16:51:28 +0000

    Lint code and add appropriate comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,6 @@
+
+// Test for 'findKeyByValue'
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,13 +9,16 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// A function "findKeyByValue" which takes in an object and a value, scans the object 
+// and returns the first key which contains the given value. 
+// If no key with that given value is found, then it should return undefined.
 
 const findKeyByValue = function(givenObj, desiredVal) {
   let result = "";
   let values = Object.values(givenObj);
   let keys = Object.keys(givenObj);
 
-  for(let i = 0; i < values.length; i++) {
+  for (let i = 0; i < values.length; i++) {
     if (values[i] === desiredVal) {
       result += keys[i];
       return result;
@@ -21,6 +27,7 @@ const findKeyByValue = function(givenObj, desiredVal) {
 };
 
 
+// Testing 'findKeyByValue'
 
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
@@ -34,5 +41,6 @@ assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wir"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
 
+// Exporting 'findKeyByValue'
 module.exports = findKeyByValue;
 
