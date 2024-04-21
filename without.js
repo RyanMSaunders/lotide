6f59cf9585d218aa8819bf4a34d25e2c3d9df213
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-21 23:40:34 +0000

    Remove unit testing, so file only includes function and export

--- without.js
+++ without.js
@@ -1,25 +1,3 @@
-// Tests for 'without' function
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 
 // A function 'without' which will return a subset of a given array, removing unwanted elements.
 
@@ -34,20 +12,6 @@ const without = function(source, itemsToRemove) {
   }
   return newArr;
 };
-
- 
-// Testing 'without' function
-
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-
-const words = ["hello", "world", "lighthouse"];
-// Making sure the original array was not altered by the without function
-without(words, ["lighthouse"]); 
-
-assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => should PASS
-
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
 
 
 // Exporting 'without'
