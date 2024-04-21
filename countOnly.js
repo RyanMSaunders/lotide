1d5789068e758db548905e06bb1b1fee7e5ba2c1
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-21 23:37:39 +0000

    Remove unit testing, so file only includes function and export

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,3 @@
-// Test for 'countOnly'
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 
 // A function 'countOnly' that takes in a collection of items and return 
 // counts for a specific subset of those items.
@@ -26,27 +17,6 @@ const countOnly = function(allItems, itemsToCount) {
   }
   return results;
 };
-
-
-// Testing 'countOnly'
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
 
 // Exporting 'countOnly'
 
