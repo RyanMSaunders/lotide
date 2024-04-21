62230a2f6369f1ee42e128b531a58205441e218f
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-21 23:39:01 +0000

    Remove unit testing, so file only includes function and export

--- flatten.js
+++ flatten.js
@@ -1,24 +1,3 @@
-// Tests for 'flatten'
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
 
 // A function flatten which will take in an array containing elements including
 // nested arrays of elements, and return a "flattened" version of the array. Limited
@@ -38,12 +17,6 @@ const flatten = function(arr) {
   }
   return newArr;
 };
-
-
-// Testing 'flatten'
-
-flatten([1, 2, [3, 4], 5, [6]]);
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
 
 // Exporting 'flaten'
 module.exports = flatten;
