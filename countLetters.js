8f82382320dd0455abbad08aa17efb44c9c8e4bc
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-21 23:37:18 +0000

    Remove unit testing, so file only includes function and export

--- countLetters.js
+++ countLetters.js
@@ -1,12 +1,3 @@
-// Test for 'countLetters'
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 
 // A function 'countLetters' that takes in a sentence (as a string) and then 
 // returns a count of each of the letters in that sentence.
@@ -22,16 +13,6 @@ const countLetters = function(inputStr) {
   }
   return results;
 };
-
-
-// Testing 'countLetters'
-
-const result1 = countLetters("Ryann");
-
-assertEqual(result1["R"], 1);
-assertEqual(result1["y"], 1);
-assertEqual(result1["a"], 1);
-assertEqual(result1["n"], 2);
 
 // Exporting 'countLetters'
 
