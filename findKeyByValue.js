e385844bb206f377a8f5867253d652f829c82b90
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-21 23:38:40 +0000

    Remove unit testing, so file only includes function and export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,14 +1,4 @@
 
-// Test for 'findKeyByValue'
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // A function "findKeyByValue" which takes in an object and a value, scans the object 
 // and returns the first key which contains the given value. 
 // If no key with that given value is found, then it should return undefined.
@@ -25,21 +15,6 @@ const findKeyByValue = function(givenObj, desiredVal) {
     }
   }
 };
-
-
-// Testing 'findKeyByValue'
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wir"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
 
 // Exporting 'findKeyByValue'
 module.exports = findKeyByValue;
