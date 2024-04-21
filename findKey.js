94369215b2b681fd69ecc59e4c0cd56e58974c85
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-21 23:38:18 +0000

    Remove unit testing, so file only includes function and export

--- findKey.js
+++ findKey.js
@@ -1,13 +1,3 @@
-// Test for 'findKey'
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 
 // A function 'findKey' which takes in an object and a callback, scans
 // the object and returns the first key for which the callback 
@@ -23,27 +13,6 @@ const findKey = function(object, callback) {
     }
   }
 };
-
-
-// Testing 'findKey'
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma"); // => should PASS
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 3 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 3 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), undefined); // => should PASS
 
 // Exporting 'findKey'
 module.exports = findKey;
